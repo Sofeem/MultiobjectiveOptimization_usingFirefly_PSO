908c7ab948cfe5e1c11138bba27df6228c997825
Sofeem <32817364+Sofeem@users.noreply.github.com>
2022-10-02 02:31:06 +0300

    Update Coverage.java

--- MultiobjectFA/src/multiobjectfa/Coverage.java
+++ MultiobjectFA/src/multiobjectfa/Coverage.java
@@ -25,17 +25,12 @@ class Coverage implements Constants {public static double evaluate(Sensor sensor
                         for(int j=0; j<grid.length;j++){
               
                                 ED = EuDistance(value1,value2,i,j);
-                //System.out.print("ED = ");
-                //System.out.println(ED);
-                //System.out.print("Radius= ");
-                //System.out.println(Radius);
+                
                 
                 if ((double)(SRadius) > ED){
-                    //System.out.print("In");
+                    
                     grid[i][j] = 1;
-                    //System.out.print(i);
-                    //System.out.print(j);
-                    //System.out.println();
+                    
                 }}}}
 
 
@@ -51,15 +46,6 @@ class Coverage implements Constants {public static double evaluate(Sensor sensor
        
        covpr = cov;
        
-       //displayGrid(grid);
-      //System.out.println();
-       //System.out.print(cov);
-       //System.out.println();
-       //System.out.println(covpr);
-       
-
- //fi.get(index).covfitness=;
-
 return covpr;
 }
  public static void inlGrid(){
@@ -89,26 +75,13 @@ return covpr;
    public static double EuDistance(double Sx, double Sy, int Gx, int Gy){
     
     double Eud;
-    //System.out.print("Sx = ");
-    //System.out.println(Sx);
-    //System.out.print("Gx = ");
-    //System.out.println(Gx);
-    //System.out.print("Sy = ");
-    //System.out.println(Sy);
-    //System.out.print("Gy = ");
-    //System.out.println(Gy);
     
     double dx = (Sx-Gx);
     double dy = (Sy-Gy);
-    //System.out.print("dx = ");
-    //System.out.println(dx);
-    //System.out.print("dy = ");
-    //System.out.println(dy);
-    
+   
     double dis = (dx*dx)+(dy*dy);
     
-    //System.out.print("dis = ");
-    //System.out.println(dis);
+  
     
         Eud = Math.sqrt(dis);
         
